f0034b2427c2282f43f037a3ec635b964fa2be28
Mac Strelioff <macstrelioff@gmail.com>
2022-11-13 18:51:50 -0800

    auto

--- setup/ConectAndAssignAccount.py
+++ setup/ConectAndAssignAccount.py
@@ -1,13 +1,14 @@
 from web3 import Web3
+
 # to get key from environment
 import os
-from dotenv import load_dotenv # https://pypi.org/project/python-dotenv/
+from dotenv import load_dotenv  # https://pypi.org/project/python-dotenv/
 
 # to load environment dictionary
 load_dotenv('../.env')
 
 # api key originally from https://dashboard.alchemy.com/
-apiKey=os.environ['ALCHEMY_KEY'] # gets key from environment, alternatively one could paste their key here
+apiKey=os.environ['ALCHEMY_KEY']  # gets key from environment, alternatively one could paste their key here
 alchemy_url = f"https://eth-mainnet.g.alchemy.com/v2/{apiKey}"
 
 # connect to an ethereum node through Alchemyy
