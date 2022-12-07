f4fb92bf1e16c02d12c3f799db5885c5482d46fd
Mac Strelioff <macstrelioff@gmail.com>
2022-12-06 21:23:48 -0800

    auto

--- setup/ConectAndAssignAccount.py
+++ setup/ConectAndAssignAccount.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv  # https://pypi.org/project/python-dotenv/
 load_dotenv('../.env')
 
 # api key originally from https://dashboard.alchemy.com/
-apiKey=os.environ['ALCHEMY_KEY']  # gets key from environment, alternatively one could paste their key here
-alchemy_url = f"https://eth-mainnet.g.alchemy.com/v2/{apiKey}"
+# apiKey=os.environ['ALCHEMY_KEY']  # gets key from environment, alternatively one could paste their key here
+gapikey=os.environ['gapikey'] 
+# alchemy_url = f"https://eth-mainnet.g.alchemy.com/v2/{apiKey}"
+alchemy_url = f"https://eth-goerli.g.alchemy.com/v2/{gapikey}"
 
 # connect to an ethereum node through Alchemyy
 w3 = Web3(Web3.HTTPProvider(alchemy_url))
@@ -17,6 +19,6 @@ w3 = Web3(Web3.HTTPProvider(alchemy_url))
 print(f'connected: {w3.isConnected()}')
 
 # get account
-PRIVATE_KEY = os.environ['PRIVATE_KEY']
+PRIVATE_KEY = os.environ['PRIVATE_KEY_VENTURES_OLD']
 acct = w3.eth.account.from_key(PRIVATE_KEY)
 print(f'address: {acct.address}')
