67919345b6b941b6a425503c2d3b2730575e0d1b
Mac Strelioff <macstrelioff@gmail.com>
2022-12-18 16:52:08 -0800

    auto

--- solidity/mac-ventures-token/hardhat.config.js
+++ solidity/mac-ventures-token/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    // get from Alchemy at https://dashboard.alchemy.com/apps
+    goerli: {
+      // get test eth from https://goerlifaucet.com/
+      url: process.env.ALCHEMY_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
+    },
+    mainnet: {
+      url: process.env.ALCHEMY_MAINNET_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY_DEPLOYER]
+    }
+  },
+  etherscan: {
+    // Obtain one at https://etherscan.io/myapikey
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
