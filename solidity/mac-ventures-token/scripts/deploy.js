30f24c1ec17e9803c8496596138221cc96068cbe
Mac Strelioff <macstrelioff@gmail.com>
2022-12-18 16:22:24 -0800

    auto

--- solidity/mac-ventures-token/scripts/deploy.js
+++ solidity/mac-ventures-token/scripts/deploy.js
@@ -1,30 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Deploying contract with acconut: ", deployer.address);
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const MacVenturesToken = await hre.ethers.getContractFactory("MacVenturesToken");
+  const token = await MacVenturesToken.deploy(50000000000);
 
-  await lock.deployed();
+  await token.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Token address: ",token.address);
+
+  await hre.run("verify:verify", {
+    address: token.address,
+    constructorArguments: [50000000000]
+  });
+  
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
